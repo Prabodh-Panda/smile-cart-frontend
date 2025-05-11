246567ff4bf8a6119c1f9408045d587eb131da82
Prabodh Kumar Panda <dev.prabodhpanda@gmail.com>
2025-05-11 21:44:00 +0530

    Added feature to add products to cart

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -6,7 +6,7 @@ import PageLoader from "components/commons/PageLoader";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, without } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
@@ -15,6 +15,7 @@ const ProductList = () => {
   const [loading, setLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
+  const [cartItems, setCartItems] = useState([]);
 
   const fetchProducts = async () => {
     try {
@@ -37,9 +38,17 @@ const ProductList = () => {
     return <PageLoader />;
   }
 
+  const toggleIsInCart = slug =>
+    setCartItems(prevCartItems =>
+      prevCartItems.includes(slug)
+        ? without([slug], cartItems)
+        : [slug, ...cartItems]
+    );
+
   return (
     <div className="flex h-screen flex-col">
       <Header
+        cartItemsCount={cartItems.length}
         shouldShowBackButton={false}
         title="Smile Cart"
         actionBlock={
@@ -57,7 +66,12 @@ const ProductList = () => {
       ) : (
         <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
           {products.map(product => (
-            <ProductListItem key={product.slug} {...product} />
+            <ProductListItem
+              key={product.slug}
+              {...product}
+              isInCart={cartItems.includes(product.slug)}
+              toggleIsInCart={() => toggleIsInCart(product.slug)}
+            />
           ))}
         </div>
       )}
